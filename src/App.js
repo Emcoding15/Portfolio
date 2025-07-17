acb8c7359197eb4c241606c28b7746b6a4fb9910
EmCoding15 <jmguerra015@gmail.com>
2025-07-17 20:05:45 +0800

    Remove Skills page from portfolio

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './styles/App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
-import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -14,7 +13,6 @@ function App() {
       <Header />
       <Hero />
       <About />
-      <Skills />
       <Projects />
       <Contact />
       <Footer />
--- src/components/Header.js
+++ src/components/Header.js
@@ -44,7 +44,6 @@ const Header = () => {
           <ul>
             <li><a href="#home" onClick={() => setIsMenuOpen(false)}>Home</a></li>
             <li><a href="#about" onClick={() => setIsMenuOpen(false)}>About</a></li>
-            <li><a href="#skills" onClick={() => setIsMenuOpen(false)}>Skills</a></li>
             <li><a href="#projects" onClick={() => setIsMenuOpen(false)}>Projects</a></li>
             <li><a href="#contact" onClick={() => setIsMenuOpen(false)}>Contact</a></li>
           </ul>
